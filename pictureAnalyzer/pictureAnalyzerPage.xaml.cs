f08bf0ce6600fe31fef3d2cf98830ae7b428b1fd
Juan Martí <jmarti.dev@gmail.com>
2017-06-17 12:46:33 -0400

    Get Image Description Function
    Created a function to call the Cognitive Service API

--- pictureAnalyzer/pictureAnalyzerPage.xaml.cs
+++ pictureAnalyzer/pictureAnalyzerPage.xaml.cs
@@ -1,4 +1,10 @@
-﻿using Xamarin.Forms;
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.ProjectOxford.Vision.Contract;
+using Xamarin.Forms;
+using Microsoft.ProjectOxford.Vision;
+using System.Linq;
+using Plugin.Media;
 
 namespace pictureAnalyzer
 {
@@ -7,6 +13,17 @@ namespace pictureAnalyzer
         public pictureAnalyzerPage()
         {
             InitializeComponent();
+        }
+
+        private async Task<AnalysisResult> GetImageDescription(Stream imageStream)
+        {
+            VisionServiceClient visionClient = new VisionServiceClient(
+                "ee8f52426e87468cbff5ab0f4c19bc8f", 
+                "https://eastus2.api.cognitive.microsoft.com/vision/v1.0");
+
+            VisualFeature[] features = { VisualFeature.Tags };
+
+            return await visionClient.AnalyzeImageAsync(imageStream, features.ToList(), null);
         }
     }
 }
